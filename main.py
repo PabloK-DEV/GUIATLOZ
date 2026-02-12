c8b8f2d158c77e8afd6b6d9093c42b02e2b76c7e
pablok-dev <pablokawan25.dev@gmail.com>
2026-02-11 22:28:51 -0300

    refatorar: corrigir rotas e padronizar nomes de parâmetros

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ import requests
 
 app = FastAPI()
 
-# --- DEFINIÇÃO DA CLASSE (A estrutura do seu sistema) ---
-
-
-# --- AS ROTAS (@app.get) --- '''
+# --- AS ROTAS (@app.get) --- 
 
 @app.get("/")
 def home():
-    return {"status": "aqui", "docs": "/docs"}
+    return {"tela principal": "aqui", "docs": "/docs"}
 
-@app.get("/item/{name}")
+# --- ROTA PARA TODOS OS ITENS ---
+
+# ---   ROTA PARA ITEM ---
+@app.get("/entry/{name}")
 def read_item(name: str):
-    print('chegou aqui')
     try:
-        data = requests.get(f'https://botw-compendium.herokuapp.com/api/v3/compendium/entry/{name}')
+        data = requests.get(f'https://botw-compendium.herokuapp.com/api/v3/compendium/entry/{name.lower()}')
+        return data.json()
+    except Exception as e:
+        print (str ('e'))
+        raise HTTPException(status_code=404, detail=str(e))
+
+# --- ROTA PARA CATEGORIA ---
+@app.get("/category/{category}")
+def read_category(category: str):
+    try:
+        data = requests.get(f'https://botw-compendium.herokuapp.com/api/v3/compendium/category/{category.lower()}')
         return data.json()
     except Exception as e:
         print (str (e))
