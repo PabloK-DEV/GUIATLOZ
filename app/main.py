7b666c8e5c3b1bb72f9f9cc623882c960983cc06
JosueFS <josue.18rj@gmail.com>
2026-03-02 21:34:57 -0300

    feat(version): update API version to 0.0.5 and include root router in main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
-from fastapi import FastAPI, Depends
 import requests
-from .routers import category
-from .dependencies import get_query_token, get_token_header
+from fastapi import FastAPI, Depends
+from .routers import category, root as root_router
+from .dependencies import get_token_header
 from .internal import admin
 from app.routers import items, users
 from fastapi.middleware.cors import CORSMiddleware
 
 
-app = FastAPI(version="0.0.4")
+app = FastAPI(version="0.0.5")
 
 origins = [
     "http://localhost",
@@ -15,17 +15,19 @@ origins = [
     "http://localhost:8080",
     "http://localhost:3000",
     "http://localhost:5173",
-    "https://hyrule-wiki.vercel.app",
+    # "https://hyrule-wiki.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex="https://.*\\.vercel\\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.include_router(root_router.router)
 app.include_router(category.router)
 
 app.include_router(users.router)
