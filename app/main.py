27dfc722ff89b5a028446757cbff78e835aeece3
JosueFS <josue.18rj@gmail.com>
2026-03-02 21:02:48 -0300

    feat(cors): add CORS middleware and configure allowed origins in main.py

--- app/main.py
+++ app/main.py
@@ -4,8 +4,26 @@ from .routers import root, category
 from .dependencies import get_query_token, get_token_header
 from .internal import admin
 from app.routers import items, users
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:*",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "https://.*.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(root.router)
 app.include_router(category.router)
